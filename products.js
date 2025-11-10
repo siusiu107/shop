e0b56774dd9cc486763825985e5b1a04eeb6d86f
SIUStudio <siu46852579@gmail.com>
2025-11-10 23:12:01 +0900

    Update products.js

--- products.js
+++ products.js
@@ -1,5 +1,5 @@
 export const PRODUCTS = [
-  { id: "coin_100",  name: "이벤트 코인 100개", coins: 100, priceKRW: 5500,  bonus: 0 },
+  { id: "coin_100",  name: "이벤트 코인 100개", coins: 100, priceKRW: 1,  bonus: 0 },
   { id: "coin_250",  name: "이벤트 코인 250개", coins: 250, priceKRW: 12900, bonus: 25 },
   { id: "coin_600",  name: "이벤트 코인 600개", coins: 600, priceKRW: 29900, bonus: 100 }
 ];
